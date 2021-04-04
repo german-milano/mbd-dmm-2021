7fb4efce3ceb403ad8a8ca8f7de2184f3fb230e5
german-milano <g.milano87@gmail.com>
2021-04-04 15:56:16 -0300

    METRICS: se normalizan a ENG los comentarios; se agregan docstrings a todas las funciones

--- project/utils/metrics.py
+++ project/utils/metrics.py
@@ -4,14 +4,27 @@ from scipy.special import softmax
 
 
 def ris_weights_and_return(trajectories, device, policy_net, paths_number, behavior_policy, n):
-    # TODO (gmilano): docstrings
-    # TODO (gmilano): implementar para diferentes políticas
+    '''
+    Function to calculate the weights and returns for the RIS equation
+
+            Parameters:
+                    trajectories (list): dataset of trajectories
+                    device (torch.device): device where to run ops in
+                    policy_net (torch.nn.Model): artificial neural network
+                    path_number (int): number of complete paths in trajectories
+                    behavior_policy (dict): estimated policy from the dataset
+                    n (int): range of the trajectory
+                    
+            Returns:
+                    ws (list): weights from RIS equation
+                    gs (list): returns from RIS equation
+    '''
     
-    # Inicio pesos en 1 y retorno en 0
+    # Initialize weights and returns lists
     ws = np.ones(paths_number)
     gs = np.zeros(paths_number)
 
-    # Loop principal
+    # Main loop
     for x, path in enumerate(trajectories):
         long_ob = []
 
@@ -20,26 +33,25 @@ def ris_weights_and_return(trajectories, device, policy_net, paths_number, behav
         gs[x] = sum(rewards)
 
         for state, action in zip(states, actions):
-            state = tuple(state.numpy())    # TODO (gmilano): hacerlo fuera de la función
+            state = tuple(state.numpy())
 
             long_ob.append(state)
             
             state = (torch.tensor(state)).to(device)
             
-            # Calculo los valores para los estados-acciones de ambas politicas utiliando net y prob. calculadas
-            # TODO (gmilano): sacar p y q fuera de esta función
+            # Calculate state-actions valures for both policies
             p = policy_net(state).view(-1,1).squeeze().detach() 
-            p = softmax(p)  # TODO (gmilano): pasar a np.exp(action_vec, dtype='float64') / np.sum(np.exp(action_vec, dtype='float64').numpy(), axis=0)
+            p = softmax(p)
             p = p[action].numpy()
             q = behavior_policy[tuple(long_ob)][action]
 
-            # Calculamos el peso del batch
+            # Calculate weight for current batch
             ws[x] *= p / q
     
-            #Agregamos la accion
+            # Append action
             long_ob.append(action)
     
-            #Chequeo Horizonte
+            # Check horizon
             if len(long_ob) >= n * 2:
                 long_ob = long_ob[2:]
         
@@ -47,51 +59,62 @@ def ris_weights_and_return(trajectories, device, policy_net, paths_number, behav
 
 
 def ris_behavior_policy(trajectories, n_actions, paths_number, n):
-    # TODO (gmilano): docstrings
-    ''' Aproxima la behavior policy segun los datos'''
+    '''
+    Function to estimate the behaviour policy from the available data
 
-    # Genero e inicializo Contadores para Behavior Policy
+            Parameters:
+                    trajectories (list): dataset of trajectories
+                    n_actions (int): number of possible actions
+                    paths_number (int): number of complete paths in trajectories
+                    n (int): range of the trajectory
+                    
+            Returns:
+                    probs (dict): calculated probabilities
+    '''
+
+    # Generate and initialize counters for behaviour policy
     sa_counts = {}
     sa_counts[tuple([])] = paths_number
     s_counts = {}
     s_counts[tuple([])] = paths_number
     
-    # Diccionario de Behavior Policy con probabilidades
+    # Dict for BP probs
     probs = {}
     probs[tuple([])] = paths_number
 
-    #Loop general sobre las trayectorias 
+    # Main loop
     for _, path in enumerate(trajectories):
-        #Lista para estados y acciones de la trayectoria
+        # Trajectory's states and actions list
         long_ob = []
 
-        #Utilizo los estados y acciones del path
+        # Use states and actions from path
         states, actions, _, _, _ = list(zip(*path))
 
-        # Genero loop sobre estados y acciones dentro del batch
+        # Batch's states and actions inner loop 
         for state, action in zip(states, actions):
 
-            # Pasamos los estados de tensores a narrays
+            # Convert state tensors to arrays
             state = tuple(state.numpy())
             
-            # Agregamos el estado
+            # Append state
             long_ob.append(state)
 
-            #Chequeo si ya vi la secuencia completa y la agrego en diccionario en caso contrario
+            # Check if complete sequence was already seen
+            # Otherwise, append it to dict
             if tuple(long_ob) not in s_counts:      
                 s_counts[tuple(long_ob)] = 0.0
                 sa_counts[tuple(long_ob)] = np.zeros(n_actions)
         
-            #Actualizo valores de trayectoria al momento
+            # Update values
             s_counts[tuple(long_ob)] += 1
             sa_counts[tuple(long_ob)][action] += 1
             long_ob.append(action)
 
-            #Chequeo horizonte
+            # Check horizon
             if len(long_ob) >= n * 2:
                 long_ob = long_ob[2:]
 
-    #Calculamos las probabilidades de las trayectorias
+    # Calculate trajectories' probabilities
     for state in s_counts:
         probs[state] = sa_counts[state] / s_counts[state]
 
@@ -99,19 +122,34 @@ def ris_behavior_policy(trajectories, n_actions, paths_number, n):
 
 
 def ris_estimator(trajectories, device, policy_net, behavior_policy, n_actions, horizon, n=None, weighted=False):
-    # TODO (gmilano): docstrings
+    '''
+    Function to calculate RIS estimator
+
+            Parameters:
+                    trajectories (list): dataset of trajectories
+                    device (torch.device): device where to run ops in
+                    policy_net (torch.nn.Model): artificial neural network
+                    behavior_policy (dict): estimated policy from the dataset
+                    n_actions (int): number of possible actions
+                    horizon (int): number of elements for a complete trajectory
+                    n (int): range of the trajectory
+                    weighted (boolean): ctrl var for weighted RIS calculation
+                    
+            Returns:
+                    ris (float): RIS estimator value
+    '''
 
     n_val = horizon
     if n is not None:
         n_val = n
 
-    # Numero de trayetorias a utilizar
+    # Number of trajectories to use
     paths_number = len(trajectories)
 
-    # Calculo los pesos para cada una de las trayectorias y sus rewards (se hace junto para aprovechar el loop)
+    # Calculate trajectories' weights and returns
     ws, gs = ris_weights_and_return(trajectories, device, policy_net, paths_number, behavior_policy, n_val)
 
-    # Caclulamos valor RIS(n)
+    # Compute RIS(n) value
     ris = np.dot(gs, ws)
     ris = ris / paths_number
 
